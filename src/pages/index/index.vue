38beb778d3c7e3b7806bfcea4343705b51a58609
Marshall <57112423+Xy2002@users.noreply.github.com>
2022-11-03 11:50:43 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class="index">
+    <Counter />
+  </view>
+</template>
+
+<script setup lang="ts">
+import './index.scss'
+import Counter from '../../components/Counter.vue'
+
+</script>
